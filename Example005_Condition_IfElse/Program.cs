459848dc075209ffbfe2d275b68d0e7a74dd841b
Dmitry Budyakov <dnikbu@yandex.ru>
2022-08-30 17:08:07 +0300

    revised Example005_Condition_IfElse про Машу

--- Example005_Condition_IfElse/Program.cs
+++ Example005_Condition_IfElse/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задача на ветвление
 // Особо поприветствовать пользователя, если его зовут Маша
 Console.Write("Имя пользователя: ");
-string username = Console.ReadLine().ToLower(); // все вводимые имена будут переводиться в нижний регистр
+string username = Console.ReadLine(); 
 
-if (username == "маша")
+if (username.ToLower() == "маша")   // все вводимые имена будут переводиться в нижний регистр
 {
     Console.WriteLine("Ура! Это же МАША!");
 }
