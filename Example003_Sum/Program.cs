991e3495205b485110346bc63f8178f8c4ec504c
Dmitry Budyakov <dnikbu@yandex.ru>
2022-08-30 14:11:51 +0300

    Added Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сумма двух целых чисел
+int numberA = 3;
+Console.WriteLine(numberA);
+int numberB = 5;
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+
+Console.Write("Сумма: ");
+Console.WriteLine(result);
+
+
