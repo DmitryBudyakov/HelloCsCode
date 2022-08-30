5d0df77ec591e2e3504dad2aa4c2b750449ae5b1
Dmitry Budyakov <dnikbu@yandex.ru>
2022-08-30 14:27:48 +0300

    added Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿// Деление 2-х чисел
+Console.WriteLine("Деление 2-х чисел");
+double numberA = 12;
+Console.WriteLine(numberA);
+double numberB = 5;
+Console.WriteLine(numberB);
+double result = numberA / numberB;
+
+Console.WriteLine(result);
