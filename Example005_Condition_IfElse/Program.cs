173abc155df979b62597487a58b822f8e3242017
Dmitry Budyakov <dnikbu@yandex.ru>
2022-08-30 17:04:49 +0300

    added Example005_Condition_IfElse про Машу

---
+++ Example005_Condition_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача на ветвление
+// Особо поприветствовать пользователя, если его зовут Маша
+Console.Write("Имя пользователя: ");
+string username = Console.ReadLine().ToLower(); // все вводимые имена будут переводиться в нижний регистр
+
+if (username == "маша")
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
