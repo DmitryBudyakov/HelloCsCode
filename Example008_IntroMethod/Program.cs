d9a0a52f07012b8e00ef2978d5d41d7fd495c12a
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-02 19:35:40 +0300

    Example008_IntroMethod - с добавлением функции

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -1,26 +1,33 @@
 ﻿// задача поиска максимума из 9 гирь
-// традиционный способ
+// с использованием функций - вар.1
 Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
 int a1 = 15;
 int b1 = 25;
-int c1 = 5;
-int a2 = 10;
+int c1 = 5002;
+int a2 = 1015;
 int b2 = 14;
 int c2 = 50;
 int a3 = 311;
-int b3 = 24;
+int b3 = 24081;
 int c3 = 13;
 
-int max = a1;
-if (b1 > max) max = b1;
-if (c1 > max) max = c1;
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
 
-if (a2 > max) max = a2;
-if (b2 > max) max = b2;
-if (c2 > max) max = c2;
-
-if (a3 > max) max = a3;
-if (b3 > max) max = b3;
-if (c3 > max) max = c3;
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3)
+);
 
 Console.WriteLine(max);
