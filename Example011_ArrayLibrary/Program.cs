8f4c343e49a303e97eeed37ea904b56ab480c7cf
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-06 19:54:09 +0300

    Example011_ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,52 @@
+﻿void FillArray(int[] collection) // метод создаёт массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)  // метод печати массива
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        if (position == count -1) Console.WriteLine(col[position]);
+        else Console.Write($"{col[position]}, ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find) // поиск индекса с икомым значением
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;  // на случай, если искомого значения не будет в массиве
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;  // выход из поиска при первом найденном
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+int numToFind = 4;
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine($"{numToFind}");
+Console.WriteLine();
+
+int pos = IndexOf(array, numToFind);
+Console.WriteLine(pos);
+
