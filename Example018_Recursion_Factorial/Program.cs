913cd207b2bd0e5a665dc4ecbef56df08439543b
Dmitry Budyakov <dnikbu@yandex.ru>
2022-09-20 15:29:03 +0300

    Factorial - с рекурсией

---
+++ Example018_Recursion_Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// рекурсия
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+Console.Clear();
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); // 1*2*3 = 6
+}
