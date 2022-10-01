5a6f1c983443ce85a9a29d470461130a415c5a79
Dmitry Budyakov <dnikbu@yandex.ru>
2022-10-01 23:09:48 +0300

    Example019_Recursion_Fibbonacci

---
+++ Example019_Recursion_Fibbonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿// числа Фибоначчи
+
+decimal FibonacciRec(int n)
+{
+    // if  (n == 0 || n == 1) return 1;
+    // else return Fibinacchi(n-1) + Fibinacchi(n-2);
+    return (n == 0 || n == 1) ? 1 : FibonacciRec(n - 1) + FibonacciRec(n - 2);
+}
+
+for (int i = 0; i < 5; i++)
+{
+    // Console.WriteLine($"{i}: {FibonacciRec(i)}");
+    Console.Write($"{FibonacciRec(i)} ");
+}
+Console.WriteLine();
+
