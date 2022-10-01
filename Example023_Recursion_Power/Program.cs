b8b700bea7b47a527f83b28c0b574597feafa342
Dmitry Budyakov <dnikbu@yandex.ru>
2022-10-01 23:12:37 +0300

    Example023_Recursion_Power

---
+++ Example023_Recursion_Power/Program.cs
@@ -0,0 +1,19 @@
+﻿// Возведение в степень
+
+int PowerRec(int a, int n)
+{
+    // if (n == 0) return 1;
+    // else return a * PowerRec(a, n - 1);
+    return (n == 0) ? 1 : a * PowerRec(a, n - 1);
+}
+
+int PowerRecMath(int a, int n)
+{
+    // return (n == 0) ? 1 : a * PowerRec(a, n - 1);
+    if (n == 0) return 1;
+    if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return a * PowerRecMath(a, n - 1);
+
+}
+Console.WriteLine(PowerRec(3, 3));
+Console.WriteLine(PowerRecMath(3, 3));
