8c1183e10827945c3b52852fa4ba55646f9aa3c0
Dmitry Budyakov <dnikbu@yandex.ru>
2022-10-01 23:14:21 +0300

    Example026_Recursion_Towers

---
+++ Example026_Recursion_Towers/Program.cs
@@ -0,0 +1,10 @@
+﻿// пирамидки
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, on, some, count -1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count -1);
+}
+
+Towers();
