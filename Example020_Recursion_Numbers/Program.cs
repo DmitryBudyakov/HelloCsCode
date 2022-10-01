6cd42e326521c3a1a44eccec962cea6d0fa6faab
Dmitry Budyakov <dnikbu@yandex.ru>
2022-10-01 23:10:39 +0300

    Example020_Recursion_Numbers

---
+++ Example020_Recursion_Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿// Собрать строку с числами от a до b, a <= b
+
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return String.Empty;
+}
+
+// Собрать строку с числами от a до b, a >= b
+string NumbersReverseRec(int a, int b)
+{
+    if (a <= b) return NumbersReverseRec(a + 1, b) + $"{a} ";
+    else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(1, 10));
+Console.WriteLine(NumbersReverseRec(1, 10));
